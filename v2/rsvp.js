4c85fe18259de2f58727c461a57ebbd117adb752
Niko <niko.delarich@gmail.com>
2018-02-16 17:17:35 +0100

    Cleanup V2 RSVP

---
+++ v2/rsvp.js
@@ -0,0 +1,37 @@
+function rsvpIinit() {
+    var rsvpSteps = [
+        {
+            id: "nameAndEmail",
+        },
+        {
+            id: "attending",
+        },
+        {
+            id: "team",
+        },
+        {
+            id: "song",
+        },
+        {
+            id: "honeymoon",
+        },
+        {
+            id: "advice",
+        },
+        {
+            id: "hotel",
+        },
+        {
+            id: "transport",
+        },
+        {
+            id: "message",
+        },
+    ];
+
+    var activateStep = function(idx) {
+        
+    };
+
+    activateStep(1);
+}
