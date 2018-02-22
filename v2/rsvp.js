e72742facb808876ef0f69bdff2e1065cffc7e0d
Niko <niko.delarich@gmail.com>
2018-02-22 15:07:54 +0100

    (Mostly) working signup/in & data storage

--- v2/rsvp.js
+++ v2/rsvp.js
@@ -1,24 +1,83 @@
 function rsvpInit() {
-
-    window.activateStep = function(id) {
+    window.setActiveStep = function(next) {
       var previous = $(".active-item");
-      var next = $("#" + id);
       
       if(previous) {
         previous.addClass("saving-item").removeClass("active-item");
-        previous.find('input, textarea, button, select').attr('disabled','disabled');
+        // previous.find('input, textarea, button, select').attr('disabled','disabled');
       }
     
       next.removeClass("ng-hide").addClass("active-item");
       next[0].scrollIntoView({behavior: "smooth", block: "center"})
-      next.find('input, textarea, button, select').removeAttr('disabled');
+      // next.find('input, textarea, button, select').removeAttr('disabled');
     };
+    
+    var setData = function(data) {
+      console.log(JSON.stringify(data));
+    };
+    
+    firebase.auth().signOut(); //XXX
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user.email + " signed in!");
+        firebase.database().ref('/users/' + user.uid).once('value').then(function(snapshot) {
+          var data = snapshot.val();
+          setData(data);
+        });
+        
+      } else {
+        console.log("Signed out!");
+        //TODO reload page?
+      }
+    }); 
 
-    function nextStep(ev) {
+    function onSubmit(ev) {
+      var current = $(this).closest('.form-item');
+      var form = $(this).closest('form');
+      var data = form.serializeArray().reduce(function(acc,cur) { acc[cur.name] = cur.value; return acc; }, {});
+      
+      if(current[0].id == "nameAndEmail") {
+        var email = $("#email").val();
+        
+        /* Log In or create new user */
+        firebase.auth().signInWithEmailAndPassword(email, md5(email))
+          .catch(function(error) {
+            if(error.code == "auth/user-not-found") {
+              console.log("Creating new user...");
+              firebase.auth().createUserWithEmailAndPassword(email, md5(email))
+                .catch(function(error) {
+                  console.log(error.code + " - " + error.message);
+                  alert(error.message);
+                }).then(function() {
+                  var user = firebase.auth().currentUser;
+                  firebase.database().ref('users/' + user.uid).set(data);
+                });
+            } else {
+              console.log(error.code + " - " + error.message);
+              alert(error.message);
+            }
+          });
+      } else {
+        var user = firebase.auth().currentUser;
+        firebase.database().ref('users/' + user.uid).update(data);
+      }
+      
+      var next = current.next();
+      if(!next || next.hasClass("active-item")) return;
+      
       ev.preventDefault();
-      var next = $(this).closest('.form-item').next();
-      if(next) activateStep(next[0].id);
-    };
+      setActiveStep(next);
+    }
+    
+    function activateStep(ev) {
+      var item = $(this).closest('.form-item');
+      if(!item || item.hasClass("active-item")) return;
+      
+      ev.preventDefault();
+      setActiveStep(item);
+    }
 
-    $(".form-item form").submit(nextStep);
+    $(".form-item form").submit(onSubmit);
+    $(".form-item").click(activateStep);
 }
