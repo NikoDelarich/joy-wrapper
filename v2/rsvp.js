36be5a4e51af6d5cfc54259e1d5c281583fc4aed
Niko <niko.delarich@gmail.com>
2018-02-22 16:17:04 +0100

    Almost done!

--- v2/rsvp.js
+++ v2/rsvp.js
@@ -13,10 +13,38 @@ function rsvpInit() {
     };
     
     var setData = function(data) {
-      console.log(JSON.stringify(data));
+      var textFields = $(
+        ".form-item textarea, " +
+        ".form-item input[type=text], " +
+        ".form-item input[type*=name], " +
+        ".form-item input[type=email]" +
+      "");
+      
+      for(i = 0; i < textFields.length; i++) {
+        var name = $(textFields[i]).attr('name');
+        if(!data.hasOwnProperty(name)) continue;
+        
+        var elem = $("[name=" + name + "]");
+        elem.val(data[name]);
+        elem.closest('.form-item').removeClass("ng-hide").addClass("saving-item");
+      }
+      
+      var radios = $(".form-item .choices-container > label:first-child > input[type=radio]:first-child");
+      for(i = 0; i < radios.length; i++) {
+        var name = $(radios[i]).attr('name');
+        if(!data.hasOwnProperty(name)) continue;
+        
+        var elem = $("[name*=" + name + "][value=" + data[name] + "]");
+        elem.attr("checked", "checked");
+        elem.closest('.form-item').removeClass("ng-hide").addClass("saving-item");
+      }
+      
+      if($(".form-item.ng-hide:not(#saveAndContinue)").length == 0) {
+        $("#saveAndContinue").removeClass("ng-hide").addClass("saving-item");
+      }
     };
     
-    firebase.auth().signOut(); //XXX
+    // firebase.auth().signOut(); //XXX
     
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
@@ -57,6 +85,9 @@ function rsvpInit() {
               console.log(error.code + " - " + error.message);
               alert(error.message);
             }
+          }).then(function() {
+            var user = firebase.auth().currentUser;
+            firebase.database().ref('users/' + user.uid).update(data);
           });
       } else {
         var user = firebase.auth().currentUser;
