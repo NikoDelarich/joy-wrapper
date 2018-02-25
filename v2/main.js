487f776f5e3e69c072a553c784713097adff7472
Niko <niko.delarich@gmail.com>
2018-02-25 23:10:02 +0100

    Tawk fix?

--- v2/main.js
+++ v2/main.js
@@ -131,7 +131,7 @@ function mainInit() {
       
       $(".fadeInOnLoad").removeClass("invisible");
       
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      window.Tawk_API=Tawk_API||{}, window.Tawk_LoadStart=new Date();
       (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
--- v2/rsvp.js
+++ v2/rsvp.js
@@ -237,7 +237,7 @@ function rsvpInit() {
     
     
     window.requestAnimationFrame(function() {
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      window.Tawk_API=Tawk_API||{}, window.Tawk_LoadStart=new Date();
       (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
