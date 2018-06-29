b2746d8966108a94fe9d0708aaf321e6a6d890d5
Niko <niko.delarich@gmail.com>
2018-06-29 15:58:59 +0200

    First working version

--- v2/admin.js
+++ v2/admin.js
@@ -132,22 +132,7 @@ function adminInit() {
       } else {
         console.log("Signed out!");
       }
-    }); 
-    
-    var encrypt = function(data, pw, idx) {
-      var key = aesjs.utils.hex.toBytes(sha256(pw));
-      var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(idx));
-      return aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(data)));
-    };
-    
-    var decrypt = function(str, pw, idx) {
-      var key = aesjs.utils.hex.toBytes(sha256(pw));
-      var data = aesjs.utils.hex.toBytes(str);
-      var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(idx));
-      var decryptedBytes = aesCtr.decrypt(data);
-      
-      return aesjs.utils.utf8.fromBytes(decryptedBytes);
-    };
+    });
     
     $("#cryptSection > button").on("click", function() {
       $.ajax("https://api.backblazeb2.com/b2api/v1/b2_authorize_account", {
